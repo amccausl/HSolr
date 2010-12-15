ab818499fa462c97bacc7a65de023d4009fcafbe
Alex McCausland <alex.mccausland@gmail.com>
2010-12-15 17:41:38 -0500

    Updated test to match new data structures and API.

--- testsuite/tests/read-solr-query.hs
+++ testsuite/tests/read-solr-query.hs
@@ -2,7 +2,7 @@
 import Text.XML.HXT.Core
 import Text.XML.HXT.Arrow.XmlState.SystemConfig
 import Control.Arrow.ArrowList
-import Network.Search.Solr.Data
+import Network.Search.Data
 import Data.Time
 import Data.UUID
 import Locale
@@ -15,26 +15,26 @@ play2 = do
 
 findResponse = getChildren >>> isElem >>> hasName "response"
 
-getDocs :: (ArrowXml a) => a XmlTree SolrDoc
+getDocs :: (ArrowXml a) => a XmlTree SearchDoc
 getDocs = getChildren >>>
     isElem >>> hasName "result" >>>
     getChildren >>>
     isElem >>> hasName "doc" >>>
     processDoc
 
-processDoc :: (ArrowXml a) => a XmlTree SolrDoc
+processDoc :: (ArrowXml a) => a XmlTree SearchDoc
 processDoc = (getChildren >>> processField) >. id
-  where processField = (getAttrl >>> getChildren >>> getText) &&& getSolrData
+  where processField = (getAttrl >>> getChildren >>> getText) &&& getSearchData
 
-getSolrData :: (ArrowXml a) => a XmlTree SolrData
+getSolrData :: (ArrowXml a) => a XmlTree SearchData
 getSolrData = processType "str" (\x -> tryUUID x)
-          <+> processType "bool" (\x -> SolrBool (x == "true"))
-          <+> processType "float" (\x -> SolrFloat (read x))
-          <+> processType "date" (\x -> SolrDate (readTime defaultTimeLocale "%FT%TZ" x))
-          <+> processType "int" (\x -> SolrInt (read x))
-          <+> ((isElem >>> hasName "arr") >>> (getChildren >>> getSolrData) >. SolrArr)
+          <+> processType "bool" (\x -> SearchBool (x == "true"))
+          <+> processType "float" (\x -> SearchFloat (read x))
+          <+> processType "date" (\x -> SearchDate (readTime defaultTimeLocale "%FT%TZ" x))
+          <+> processType "int" (\x -> SearchInt (read x))
+          <+> ((isElem >>> hasName "arr") >>> (getChildren >>> getSolrData) >. SearchArr)
   where processType t f = isElem >>> hasName t >>> getChildren >>> getText >>> arr f
         tryUUID str = case fromString str of
-            Just uuid -> SolrId uuid
-            Nothing -> SolrStr str
+            Just uuid -> SearchId uuid
+            Nothing -> SearchStr str
 
