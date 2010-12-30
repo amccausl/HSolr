02ce24da2a74d9da7fdca5ce5d474ad57c06d2a1
Alex McCausland <alex.mccausland@gmail.com>
2010-12-30 01:53:39 -0500

    Added test cases for search data types and utility functions.

---
+++ testsuite/tests/Network/Search/TestData.hs
@@ -0,0 +1,79 @@
+
+module Network.Search.TestData where
+
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck (testProperty)
+
+import Test.QuickCheck
+import Test.HUnit
+
+-- Import common libraries to facilitate tests
+import qualified Data.Map as Map
+import Data.Char
+import Data.List
+import Data.Time
+import Locale
+import Data.Ranged
+
+-- Import library to test
+import Network.Search.Data
+import Network.Search.Solr
+
+-- * Data instances to test
+
+doc1 = [ ("cat",SearchArr [SearchStr "electronics",SearchStr "connector"])
+       , ("features",SearchArr [SearchStr "car power adapter, white"])
+       , ("id",SearchStr "F8V7067-APL-KIT")
+       , ("inStock",SearchBool False)
+       , ("manu",SearchStr "Belkin")
+       , ("manufacturedate_dt",SearchDate (readTime defaultTimeLocale "%FT%TZ" "2005-08-01T16:30:25Z"))
+       , ("name",SearchStr "Belkin Mobile Power Cord for iPod w/ Dock")
+       , ("popularity",SearchInt 1)
+       , ("price",SearchFloat 19.95)
+       , ("weight",SearchFloat 4.0)
+       ]
+
+-- Int
+int1 = SearchInt 1
+int2 = SearchInt 2
+int3 = SearchInt 0
+
+-- Float
+float1 = SearchFloat 19.95
+float2 = SearchFloat 19.96
+float3 = SearchFloat 19.94
+
+-- Bool
+boolT = SearchBool True
+boolF = SearchBool False
+
+-- Str
+str1 = SearchStr "Belkin"
+str2 = SearchStr "electronics"
+
+-- Date
+
+-- * Test getFacetField
+
+test_getFacetField1 = getFacetField (RangeFacet "fieldName" (Range (BoundaryBelow int1) (BoundaryAbove int2))) @?= "fieldName"
+test_getFacetField2 = getFacetField (ValueFacet "fieldName" int1) @?= "fieldName"
+
+-- * Test getFieldValue
+
+test_getFieldValue1 = getFieldValue "manu" doc1 @?= Just str1
+test_getFieldValue2 = getFieldValue "missingName" doc1 @?= Nothing
+
+-- * Test getFieldValues
+
+test_getFieldValues1 = getFieldValues "price" doc1 @?= [SearchFloat 19.95]
+test_getFieldValues2 = getFieldValues "cat" doc1 @?= [SearchStr "electronics",SearchStr "connector"]
+
+-- * Test matchesFacet
+
+test_matchesFacet_strArrayMatch = matchesFacet (ValueFacet "cat" str2) doc1 @?= True
+test_matchesFacet_strArrayMiss = matchesFacet (ValueFacet "cat" str1) doc1 @?= False
+test_matchesFacet_strMatch = matchesFacet (ValueFacet "manu" str1) doc1 @?= True
+test_matchesFacet_strMiss = matchesFacet (ValueFacet "manu" str2) doc1 @?= True
+
+
--- testsuite/tests/RunTests.hs
+++ testsuite/tests/RunTests.hs
@@ -6,13 +6,31 @@ import Test.Framework.Providers.QuickCheck (testProperty)
 import Test.QuickCheck
 import Test.HUnit
 
+import qualified Network.Search.TestData as Data
 import qualified Network.Search.TestSolr as Solr
 
-main = defaultMain [ testGroup "parseSolrResult" [ testCase "docs1" Solr.test_parseSolrResult_docs1
+main = defaultMain [ testGroup "Data:getFacetField" [ testCase "1" Data.test_getFacetField1
+                                                    , testCase "2" Data.test_getFacetField2
+                                                    ]
+                   , testGroup "Data:getFieldValue" [ testCase "1" Data.test_getFieldValue1
+                                                    , testCase "2" Data.test_getFieldValue2
+                                                    ]
+                   , testGroup "Data:getFieldValues" [ testCase "1" Data.test_getFieldValues1
+                                                     , testCase "2" Data.test_getFieldValues2
+                                                     ]
+                   , testGroup "Data:matchesFacet" [ testCase "strArrayMatch" Data.test_matchesFacet_strArrayMatch
+                                                   , testCase "strArrayMiss" Data.test_matchesFacet_strArrayMiss
+                                                   , testCase "strMatch" Data.test_matchesFacet_strMatch
+                                                   , testCase "strMiss" Data.test_matchesFacet_strMiss
+                                                   ]
+                   , testGroup "Solr:parseSolrResult" [ testCase "docs1" Solr.test_parseSolrResult_docs1
+                                                      , testCase "count1" Solr.test_parseSolrResult_count1
+                                                      , testCase "facet1" Solr.test_parseSolrResult_facet1
+                                                      , testCase "refinements1" Solr.test_parseSolrResult_refinements1
+                                                      ]
+                   , testGroup "Solr:toQueryMap" [ testCase "sort1" Solr.test_toQueryMap_sort1
+                                                 , testCase "sort2" Solr.test_toQueryMap_sort2
+                                                 , testCase "sort3" Solr.test_toQueryMap_sort3
                                                  ]
-                   , testGroup "toQueryMap" [ testCase "sort1" Solr.test_toQueryMap_sort1
-                                            , testCase "sort2" Solr.test_toQueryMap_sort2
-                                            , testCase "sort3" Solr.test_toQueryMap_sort3
-                                            ]
                    ]
 
